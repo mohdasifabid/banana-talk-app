9715531436140194bdb6dd6ec931dbb36fe5ef11
asif <masifabid@outlook.com>
2021-07-13 17:00:30 +0530

    js connected

---
+++ banantalk.js
@@ -0,0 +1,9 @@
+// console.log("To check script working or not!")
+
+// alert ("dont disturb man, it works") 
+
+var username = prompt("Gotta Name!")
+// alert ("dont disturb " + username +  " it works")
+
+var welcomeMsg = "welcome dear " + username; 
+alert(welcomeMsg);
