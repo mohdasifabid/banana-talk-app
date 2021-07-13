b7a881aace403424b25dd1ca43dc2c6dfee5529c
asif <masifabid@outlook.com>
2021-07-13 19:01:10 +0530

    Input-Output Part done

--- banantalk.js
+++ banantalk.js
@@ -2,8 +2,26 @@
 
 // alert ("dont disturb man, it works") 
 
-var username = prompt("Gotta Name!")
+// var username = prompt("Gotta Name!")
 // alert ("dont disturb " + username +  " it works")
 
-var welcomeMsg = "welcome dear " + username; 
-alert(welcomeMsg);
+// var welcomeMsg = "welcome dear " + username; 
+// alert(welcomeMsg);
+
+
+var btnTranslate = document.querySelector("#btnTranslate")
+btnTranslate.addEventListener("click", function() {
+    console.log("Clicked")
+    console.log("Input", box1.value);
+    box2.innerText = "Sorry, we are closed!" + box1.value;
+})
+
+var box1 = document.querySelector("#box1")
+console.log(box1)
+
+var box2 = document.querySelector("#box2")
+   
+    //  box2.innerText = "Sorry, we are closed!"
+
+
+
